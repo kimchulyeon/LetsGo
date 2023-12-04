e6406bb309558587747ecb3ca63f127784d6fd4c
kimchulyeon <guinness987@gmail.com>
2023-12-04 17:32:24 +0900

    Initial Commit

---
+++ LetsGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsGo
+//
+//  Created by chulyeon kim on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
