9acf1c9babe25337bd7b08d43faf57b720a61a5f
chulyeon <guinness987@gmail.com>
2023-12-10 23:19:32 +0900

    todo

--- LetsGo/Presentation/InitialSetting/ViewModel/ConfirmBottomSheetVM.swift
+++ LetsGo/Presentation/InitialSetting/ViewModel/ConfirmBottomSheetVM.swift
@@ -38,6 +38,7 @@ class ConfirmBottomSheetVM {
         
         input.confirmButtonTapped
             .subscribe { [weak self] _ in
+                #warning("확인 버튼 눌렀을 때 선택된 장소 정보 가지고 있는지? 선택된 장보 정소 보관 확인")
                 print(self?.selectedLocation)
             }
             .disposed(by: bag)
